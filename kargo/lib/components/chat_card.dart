40eda8a975ca73b25f86769b41cddf09dbf736e8
Michael Girges <maicheltito1@gmail.com>
2022-12-24 02:31:41 +0200

    chats list ui

--- kargo/lib/components/chat_card.dart
+++ kargo/lib/components/chat_card.dart
@@ -1,11 +1,64 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:kargo/models/chat_users_model.dart';
 
-class ChatCard extends StatelessWidget {
-  const ChatCard({super.key});
+class ChatCard extends StatefulWidget {
+  ChatUser user;
+  bool isMessageRead;
 
+  ChatCard({required this.user, required this.isMessageRead});
+  
+  @override
+  State<ChatCard> createState() => ChatCardState();
+}
+
+class ChatCardState extends State<ChatCard> {
+  
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListTile(
+      onTap: (){},
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(widget.user.imageUrl),
+        maxRadius: 30,
+      ),
+      title: Text(widget.user.name, style: const TextStyle(fontSize: 16),),
+      subtitle: Text(widget.user.messageText, style: TextStyle(fontSize: 13,color: Colors.grey.shade600, fontWeight: widget.isMessageRead?FontWeight.bold:FontWeight.normal),),
+      trailing: Text(widget.user.time, style: TextStyle(fontSize: 12, fontWeight: widget.isMessageRead?FontWeight.bold:FontWeight.normal),),
+      // child: Container(
+      //   padding: const EdgeInsets.only(left: 16,right: 16,top: 10,bottom: 10),
+      //   child: Row(
+      //     children: [
+      //       Expanded(
+      //         child: Row(
+      //           children: [
+      //             CircleAvatar(
+      //               backgroundImage: NetworkImage(widget.user.imageUrl),
+      //               maxRadius: 30,
+      //             ),
+      //             const SizedBox(width: 16,),
+      //             Expanded(
+      //               child: Container(
+      //                 color: Colors.transparent,
+      //                 child: Column(
+      //                   crossAxisAlignment: CrossAxisAlignment.start,
+      //                   children: [
+      //                     Text(widget.user.name, style: const TextStyle(fontSize: 16),),
+      //                     const SizedBox(height: 16,),
+      //                     Text(widget.user.messageText, style: TextStyle(fontSize: 13,color: Colors.grey.shade600, fontWeight: widget.isMessageRead?FontWeight.bold:FontWeight.normal),)
+      //                   ]
+      //                 ),
+      //               ),
+      //             )
+      //           ],
+      //         )
+      //       ),
+      //       Text(widget.user.time, style: TextStyle(fontSize: 12, fontWeight: widget.isMessageRead?FontWeight.bold:FontWeight.normal),)
+      //     ],
+      //   ),
+      // ),
+    );
   }
 }
--- kargo/lib/screens/chat_screen.dart
+++ kargo/lib/screens/chat_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:kargo/components/chat_card.dart';
 import 'package:kargo/components/my_scaffold.dart';
 import 'package:kargo/components/search_bar.dart';
+
+import '../models/chat_users_model.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -13,18 +16,28 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   bool isSearchActivated = false;
+  List<ChatUser> chatUsers = [
+    ChatUser(name: "Jane Russel", messageText: "Awesome Setup", imageUrl: "images/userImage1.jpeg", time: "Now"),
+    ChatUser(name: "Glady's Murphy", messageText: "That's Great", imageUrl: "images/userImage2.jpeg", time: "Yesterday"),
+    ChatUser(name: "Jorge Henry", messageText: "Hey where are you?", imageUrl: "images/userImage3.jpeg", time: "31 Mar"),
+    ChatUser(name: "Philip Fox", messageText: "Busy! Call me in 20 mins", imageUrl: "images/userImage4.jpeg", time: "28 Mar"),
+    // ChatUser(text: "Debra Hawkins", secondaryText: "Thankyou, It's awesome", image: "images/userImage5.jpeg", time: "23 Mar"),
+    // ChatUser(text: "Jacob Pena", secondaryText: "will update you in evening", image: "images/userImage6.jpeg", time: "17 Mar"),
+    // ChatUser(text: "Andrey Jones", secondaryText: "Can you please share the file?", image: "images/userImage7.jpeg", time: "24 Feb"),
+    // ChatUser(text: "John Wick", secondaryText: "How are you?", image: "images/userImage8.jpeg", time: "18 Feb"),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(80.0),
+        preferredSize: const Size.fromHeight(80.0),
         child: Container(
-          padding: EdgeInsets.only(top: 10),
+          padding: const EdgeInsets.only(top: 10),
           child: AppBar(
             backgroundColor: Colors.white,
             titleSpacing: 0,
-            leading: IconButton(icon: Icon(Icons.arrow_back, color: Colors.black), onPressed: (){Navigator.of(context).pop();},),
+            leading: IconButton(icon: const Icon(Icons.arrow_back, color: Colors.black), onPressed: (){Navigator.of(context).pop();},),
             title: const Text("Chats", style: TextStyle(fontSize: 28, color: Colors.black, fontWeight: FontWeight.bold),),
             actions: [
               IconButton(
@@ -44,6 +57,11 @@ class _ChatScreenState extends State<ChatScreen> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             if(isSearchActivated) const SearchBar(),
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: chatUsers.length,
+              itemBuilder: (context, index) => ChatCard(user: chatUsers[index], isMessageRead: (index == 0 || index == 3)?true:false),
+            )
           ],
         )
       )
