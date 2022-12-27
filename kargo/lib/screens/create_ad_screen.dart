2a540c395af738ae52cb9b751738dac8768b1c42
PeterFahmi <rlpeterfah@gmail.com>
2022-12-27 22:47:13 +0200

    car details with images upload

--- kargo/lib/screens/create_ad_screen.dart
+++ kargo/lib/screens/create_ad_screen.dart
@@ -1,10 +1,13 @@
+import 'dart:io';
+
 import 'package:animated_custom_dropdown/custom_dropdown.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:kargo/components/my_scaffold.dart';
 import 'package:kargo/components/my_textfield.dart';
+
+import 'package:image_picker/image_picker.dart';
+import '../components/CarouselWithDots.dart';
 
 class CreateAdScreen extends StatefulWidget {
   @override
@@ -17,12 +20,14 @@ class _CreateAdScreenState extends State<CreateAdScreen> {
   static const list = ["audi", "honda", "balabizo"];
   static const carList = ["audi", "honda", "balabizo"];
   static const modelList = ["civic", "corrola", "balabizo"];
-  static const List<String> imgUrls = [
+  static const imgUrls = [
     'https://www.hdcarwallpapers.com/download/abt_sportsline_audi_tt-2880x1800.jpg',
     'https://th.bing.com/th/id/OIP.zpu1nHs3RCyeRXikR-nFGgHaFj?pid=ImgDet&w=1600&h=1200&rs=1'
   ];
-  static const fav = 0;
+  List<Image> imgs = imgUrls.map((e) => Image.network(e)).toList();
+  bool noImages = false;
 
+  final ImagePicker picker = ImagePicker();
   final manufacturerDropdownCtrl = TextEditingController(),
       modelDropdownCtrl = TextEditingController(),
       yearCtrl = TextEditingController(),
@@ -31,83 +36,86 @@ class _CreateAdScreenState extends State<CreateAdScreen> {
 
   @override
   Widget build(BuildContext context) {
+    var addButton = Container(
+        color: Colors.grey,
+        margin: EdgeInsets.only(top: 10, right: 10, left: 5, bottom: 5),
+        height: 50,
+        width: 50,
+        child: IconButton(
+          icon: Icon(
+            Icons.add,
+            size: 30,
+            color: Colors.white,
+          ),
+          onPressed: addImage,
+        ));
     var carPart = [
       Card(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-          elevation: 4,
-          color: Colors.black,
-          margin: EdgeInsets.all(10),
-          child: Stack(
-            children: [
-// child 1 of stack is the recipe image
-              ClipRRect(
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(15),
-                      topRight: Radius.circular(15)),
-                  child: CarouselSlider(
-                    options: CarouselOptions(height: 200.0),
-                    items: imgUrls.map((i) {
-                      return Builder(
-                        builder: (BuildContext context) {
-                          return Container(
-                            width: MediaQuery.of(context).size.width,
-                            margin: EdgeInsets.symmetric(horizontal: 5.0),
-                            decoration: BoxDecoration(color: Colors.amber),
-                            child: Image.network(
-                              i,
-                              height: 200,
-                              width: double.infinity,
-                              fit: BoxFit
-                                  .cover, //byzbot shakl image expand to fit box
-                            ),
-                          );
-                        },
-                      );
-                    }).toList(),
-                  )),
-// child 2 of stack is the recipe title
-
-              Positioned(
-                  right: 0,
-                  child: fav == 0
-                      ? Container(
-                          margin: EdgeInsets.all(3),
-                          width: 40.0,
-                          height: 40.0,
-                          decoration: new BoxDecoration(
-                            color: Colors.white,
-                            shape: BoxShape.circle,
-                          ),
-                        )
-                      : Container(
-                          margin: EdgeInsets.all(3),
-                          width: 40.0,
-                          height: 40.0,
-                          decoration: new BoxDecoration(
-                            color: Colors.white,
-                            shape: BoxShape.circle,
-                          ),
-                        )),
-              Positioned(
-                  right: 0,
-                  child: fav == 0
-                      ? IconButton(
-                          onPressed: null,
-                          icon: Icon(
-                            Icons.favorite_border,
-                            size: 30,
-                            color: Colors.black,
-                          ))
-                      : IconButton(
-                          onPressed: null,
-                          icon: Icon(
-                            Icons.favorite,
-                            size: 30,
-                            color: Colors.black,
-                          ))),
-            ],
-          )),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        elevation: 0,
+        color: Colors.white60,
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            CarouselWithDotsPage(
+              imgList: getImgsToShow(),
+              isURLList: false,
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                            width: 250,
+                            height: 70,
+                            child: ListView(
+                                scrollDirection: Axis.horizontal,
+                                children: (imgs
+                                    .map((e) => Stack(children: [
+                                          Container(
+                                              color: Colors.grey,
+                                              margin: EdgeInsets.only(
+                                                  top: 10,
+                                                  right: 10,
+                                                  left: 5,
+                                                  bottom: 5),
+                                              height: 50,
+                                              width: 50,
+                                              child: e),
+                                          Positioned(
+                                              right: 0,
+                                              child: Container(
+                                                margin: EdgeInsets.all(3.5),
+                                                width: 20.0,
+                                                height: 20.0,
+                                                decoration: new BoxDecoration(
+                                                  color: Colors.white,
+                                                  shape: BoxShape.circle,
+                                                ),
+                                                child: IconButton(
+                                                    padding: EdgeInsets.all(0),
+                                                    onPressed: () =>
+                                                        removeFromList(e),
+                                                    icon: Icon(
+                                                      Icons.close,
+                                                      size: 15,
+                                                      color: Colors.black,
+                                                    )),
+                                              )),
+                                        ]))
+                                    .toList()))),
+                        addButton
+                      ],
+                    ),
+                  ]),
+            ),
+            SizedBox(height: 5),
+          ],
+        ),
+      ),
       const Text('Car details', style: _header),
       const Divider(
         height: 30,
@@ -157,5 +165,99 @@ class _CreateAdScreenState extends State<CreateAdScreen> {
           child: ListView(
               padding: const EdgeInsets.all(16.0), children: [...carPart]),
         ));
+  }
+
+  List<Image> getImgsToShow() {
+    print(imgs.length);
+    if (imgs.length == 0) {
+      List<Image> placeHolder = [];
+      placeHolder.add(Image(
+          image: AssetImage('assets/images/carPlaceHolder.jpg'),
+          fit: BoxFit.fill,
+          width: double.infinity));
+      print(placeHolder);
+      return [];
+    }
+    print(imgs);
+    return imgs;
+  }
+
+  removeFromList(Image e) {
+    setState(() {
+      imgs.remove(e);
+      if (imgs.length == 0) {
+        noImages = true;
+        imgs.add(Image(
+            image: AssetImage('assets/images/carPlaceHolder.jpg'),
+            fit: BoxFit.fill,
+            width: double.infinity));
+      }
+    });
+  }
+
+  addImage() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+            title: Text('Please choose media to select'),
+            content: Container(
+              height: MediaQuery.of(context).size.height / 6,
+              child: Column(
+                children: [
+                  ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.black)),
+                    //if user click this button, user can upload image from gallery
+                    onPressed: () {
+                      Navigator.pop(context);
+                      getImage(ImageSource.gallery);
+                    },
+                    child: Row(
+                      children: [
+                        Icon(Icons.image),
+                        Text('From Gallery'),
+                      ],
+                    ),
+                  ),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.black)),
+                    //if user click this button. user can upload image from camera
+                    onPressed: () {
+                      Navigator.pop(context);
+                      getImage(ImageSource.camera);
+                    },
+                    child: Row(
+                      children: [
+                        Icon(Icons.camera),
+                        Text('From Camera'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
+  Future getImage(ImageSource media) async {
+    XFile? img = await picker.pickImage(source: media);
+
+//   copy the file to a new path
+    if (img != null)
+      setState(() {
+        if (noImages) {
+          imgs.removeAt(0);
+          noImages = false;
+        }
+        ImageProvider image = FileImage(File(img!.path));
+        imgs.add(Image(image: image, fit: BoxFit.fill, width: double.infinity));
+      });
   }
 }
