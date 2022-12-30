7e49026cc817ee015db0c61b56306b59aa0399b3
Michael Girges <maicheltito1@gmail.com>
2022-12-30 16:47:04 +0200

    images can be sent

--- kargo/lib/screens/chat_screen.dart
+++ kargo/lib/screens/chat_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
@@ -16,7 +19,7 @@ class ChatScreen extends StatefulWidget {
   State<ChatScreen> createState() => ChatScreenState();
 }
 
-class ChatScreenState extends State<ChatScreen> {  
+class ChatScreenState extends State<ChatScreen> {
   final List<types.Message> _messages = [];
   final _user = types.User(id: FirebaseAuth.instance.currentUser!.uid);
   late final String username;
@@ -28,36 +31,47 @@ class ChatScreenState extends State<ChatScreen> {
   void didChangeDependencies() {
     // TODO: implement didChangeDependencies
     super.didChangeDependencies();
-    
-    final args = ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+
+    final args =
+        ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
     username = args['username'] as String;
     chatReference = args['chatRef'] as DocumentReference;
     getChatStream();
   }
-  
+
   getChatStream() async {
     setState(() {
-      messageStream=chatReference.collection("messages").orderBy("date").snapshots();
+      messageStream =
+          chatReference.collection("messages").orderBy("date").snapshots();
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(80.0),
-        child: Container(
-          padding: const EdgeInsets.only(top: 10),
-          child: AppBar(
-            backgroundColor: Colors.white,
-            titleSpacing: 0,
-            leading: IconButton(icon: const Icon(Icons.arrow_back, color: Colors.black), onPressed: (){Navigator.of(context).pop();},),
-            title: Text(username, style: const TextStyle(fontSize: 28, color: Colors.black, fontWeight: FontWeight.bold),),
-          ),
-        )
-      ),
-      body: buildChatMessages()
-    );
+        appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(80.0),
+            child: Container(
+              padding: const EdgeInsets.only(top: 10),
+              child: AppBar(
+                backgroundColor: Colors.white,
+                titleSpacing: 0,
+                leading: IconButton(
+                  icon: const Icon(Icons.arrow_back, color: Colors.black),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+                title: Text(
+                  username,
+                  style: const TextStyle(
+                      fontSize: 28,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            )),
+        body: buildChatMessages());
   }
 
   void _addMessage(types.Message message) {
@@ -68,7 +82,7 @@ class ChatScreenState extends State<ChatScreen> {
 
   void _handleSendPressed(types.PartialText message) {
     String trimmedText = message.text.trim();
-    if(trimmedText == "") {
+    if (trimmedText == "") {
       return;
     }
 
@@ -100,22 +114,22 @@ class ChatScreenState extends State<ChatScreen> {
   //   return Container();
   // }
 
-  buildChatMessages(){    
+  buildChatMessages() {
     return StreamBuilder(
       stream: messageStream,
       builder: (context, snapshot) {
-        if(snapshot.connectionState == ConnectionState.waiting){
+        if (snapshot.connectionState == ConnectionState.waiting) {
           return const ShimmerCard();
         }
-        if(!snapshot.hasData){
+        if (!snapshot.hasData) {
           return const NoChatsComponent();
         }
         var updatedMessages = snapshot.data!.docs;
         // print("messages data="+messagesList[0].data().toString());
         addFetchedMessages(updatedMessages);
         return Chat(
-          messages: _messages, 
-          onSendPressed: _handleSendPressed, 
+          messages: _messages,
+          onSendPressed: _handleSendPressed,
           user: _user,
           onAttachmentPressed: _handleImageSelection,
         );
@@ -126,49 +140,79 @@ class ChatScreenState extends State<ChatScreen> {
   void addFetchedMessages(updatedMessages) {
     _messages.clear();
     updatedMessages.forEach((msg) {
-      // print("msg data="+msg.data().toString());
+      print("msg data=" + msg.data().toString());
       var sender;
-      if(msg.data()['sender']==FirebaseAuth.instance.currentUser!.uid) {
-        sender=_user;
+      var message;
+      if (msg.data()['sender'] == FirebaseAuth.instance.currentUser!.uid) {
+        sender = _user;
+      } else {
+        sender = types.User(id: msg.data()['sender']);
+      }
+      if (msg.data()['type'] == 'text') {
+        message = types.TextMessage(
+          author: sender,
+          createdAt: DateTime.parse(msg.data()['date'].toDate().toString())
+              .millisecondsSinceEpoch,
+          id: DateTime.now().toString(),
+          text: msg.data()['content'],
+        );
       }
       else{
-        sender=types.User(id: msg.data()['sender']);
+        message = types.ImageMessage(
+          author: sender,
+          createdAt: DateTime.parse(msg.data()['date'].toDate().toString())
+              .millisecondsSinceEpoch,
+          id: msg.data()['date'].toString(),
+          name: msg.data()['name'],
+          size: msg.data()['size'],
+          uri: msg.data()['uri'],
+        );
       }
-      
-      final textMessage = types.TextMessage(
-        author: sender,
-        createdAt: DateTime.parse(msg.data()['date'].toDate().toString()).millisecondsSinceEpoch,
-        id: DateTime.now().toString(),
-        text: msg.data()['content'],
-      );
-    
-      _messages.insert(0,textMessage);
+
+      _messages.insert(0, message);
     });
   }
 
   void _handleImageSelection() async {
     final result = await ImagePicker().pickImage(
-      imageQuality: 70,
-      maxWidth: 1440,
-      source: ImageSource.gallery
-    );
+        imageQuality: 70, maxWidth: 1440, source: ImageSource.gallery);
 
-    if(result != null){
-      final bytes = await result.readAsBytes();
-      final image = await decodeImageFromList(bytes);
-
-      final message = types.ImageMessage(
-        author: _user,
-        createdAt: DateTime.now().millisecondsSinceEpoch,
-        height: image.height.toDouble(),
-        name: result.name,
-        id: DateTime.now().toString(),
-        size: bytes.length,
-        uri: result.path,
-        width: image.width.toDouble()
-      );
-
-      _addMessage(message);
+    if (result == null) {
+      return;
     }
+    File imageFile = File(result.path);
+    final storageRef = FirebaseStorage.instance.ref();
+    final imageRef = storageRef.child('chat_images/${result.path}');
+    var uploadTask = imageRef.putFile(imageFile);
+    final snapshot = await uploadTask.whenComplete(() {});
+    print("url=" + await snapshot.ref.getDownloadURL());
+
+    final message = types.ImageMessage(
+      author: _user,
+      createdAt: DateTime.now().millisecondsSinceEpoch,
+      // height: image.height.toDouble(),
+      name: result.name,
+      id: DateTime.now().toString(),
+      size: storageRef.bucket.length,
+      uri: result.path,
+      // width: image.width.toDouble()
+    );
+    // print("image message json = " + message.toJson().toString());
+    // if(result != null){
+    //   final bytes = await result.readAsBytes();
+    //   final image = await decodeImageFromList(bytes);
+
+    //   final message = types.ImageMessage(
+    //     author: _user,
+    //     createdAt: DateTime.now().millisecondsSinceEpoch,
+    //     height: image.height.toDouble(),
+    //     name: result.name,
+    //     id: DateTime.now().toString(),
+    //     size: bytes.length,
+    //     uri: result.path,
+    //     width: image.width.toDouble()
+    //   );
+    db.sendMessage(chatReference, message.toJson());
+    _addMessage(message);
   }
 }
