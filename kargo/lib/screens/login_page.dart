14f88dcf1c40f5c40900e2bc3c409d6fbfe27585
philo-atef <philopateer.badeer@gmail.com>
2022-12-31 18:22:48 +0200

    merge

--- kargo/lib/screens/login_page.dart
+++ kargo/lib/screens/login_page.dart
@@ -12,36 +12,16 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-<<<<<<< HEAD
   Country _selectedCountry = Country(code: '1', name: 'United States', flagUrl: 'https://flagcdn.com/w320/us.png');
   List<Country> _countries = [];
  var emailController = TextEditingController();
  var passwordController = TextEditingController();
  var usernameController = TextEditingController();
-=======
-  var nameController = TextEditingController();
-  var emailController = TextEditingController();
-  var passwordController = TextEditingController();
-  var usernameController = TextEditingController();
->>>>>>> 91eb8f265592fa3192ef21397aefe07baaa3d6a3
   var phoneNumberController = TextEditingController();
   final authenticationInstance = FirebaseAuth.instance;
   var authenticationMode = 0;
 
-  void toggleAuthMode() {
-    setState(() {
-      authenticationMode = 1 - authenticationMode;
-    });
-
-    @override
-    Widget build(BuildContext context) {
-      // TODO: implement build
-      throw UnimplementedError();
-    }
-  }
-
   @override
-<<<<<<< HEAD
   void initState() {
     super.initState();
     _fetchCountries();
@@ -297,123 +277,18 @@ try {
 
 
  });
-=======
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: 480,
-        margin: EdgeInsets.only(top: 60, left: 10, right: 10),
-        child: Card(
-          elevation: 5,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ListView(
-              children: [
-                Center(
-                    child: Image(
-                  image: AssetImage('assets/images/logo.png'),
-                  width: 150,
-                  height: 100,
-                )),
-                if (authenticationMode == 1)
-                  TextField(
-                    decoration: InputDecoration(labelText: "Name"),
-                    controller: nameController,
-                    keyboardType: TextInputType.name,
-                  ),
-                TextField(
-                  decoration: InputDecoration(labelText: "Email"),
-                  controller: emailController,
-                  keyboardType: TextInputType.emailAddress,
-                ),
-                TextField(
-                  decoration: InputDecoration(labelText: "Password"),
-                  controller: passwordController,
-                  obscureText: true,
-                ),
-                if (authenticationMode == 1)
-                  TextField(
-                    decoration: InputDecoration(labelText: "Username"),
-                    controller: usernameController,
-                  ),
-                if (authenticationMode == 1)
-                  TextField(
-                    decoration: InputDecoration(labelText: "Phone Number"),
-                    controller: phoneNumberController,
-                    keyboardType: TextInputType.phone,
-                  ),
-                ElevatedButton(
-                  style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.black)),
-                  onPressed: () {
-                    loginORsignup();
-                  },
-                  child: (authenticationMode == 1)
-                      ? Text("Sign up")
-                      : Text("Login"),
-                ),
-                TextButton(
-                  onPressed: () {
-                    toggleAuthMode();
-                  },
-                  child: (authenticationMode == 1)
-                      ? Text("Login instead",
-                          style: TextStyle(color: Colors.black))
-                      : Text("Sign up instead",
-                          style: TextStyle(color: Colors.black)),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
->>>>>>> 91eb8f265592fa3192ef21397aefe07baaa3d6a3
 
-  void loginORsignup() async {
-    var name = nameController.text.trim();
-    var email = emailController.text.trim();
-    var password = passwordController.text.trim();
-    var username = usernameController.text.trim();
-    var phoneNumber = phoneNumberController.text.trim();
-    UserCredential authResult;
-    try {
-      if (authenticationMode == 1) // sign up
-      {
-        authResult =
-            await authenticationInstance.createUserWithEmailAndPassword(
-          email: email,
-          password: password,
-        );
-        await FirebaseFirestore.instance
-            .collection('users')
-            .doc(authResult.user!.uid)
-            .set({
-          'name': name,
-          'username': username,
-          'email': email,
-          'phoneNumber': phoneNumber,
-          'photoURL':
-              'https://firebasestorage.googleapis.com/v0/b/mobileapp-18909.appspot.com/o/profile_pictures%2Fimage_picker6613525771089450064.jpg?alt=media&token=b89b540f-341f-4f04-81ed-c7d0be17e960',
-          'favAds': [],
-          'myAds': [],
-          'myBids': [],
-          'chats': [],
-        });
-      } else //log in
-      {
-        authResult = await authenticationInstance.signInWithEmailAndPassword(
-          email: email,
-          password: password,
-        );
-      }
-    } catch (err) {
-      final error = err.toString();
+ }
+ else //log in
+ {authResult = await authenticationInstance.signInWithEmailAndPassword(
+ email: email,
+ password: password,
+ );
+
+
+ }
+} catch (err) {
+  final error =err.toString();
 // ScaffoldMessenger.of(context).showSnackBar(
 
 //    const SnackBar(
@@ -452,30 +327,27 @@ try {
         elevation: 0,
       ));
 
-      // showDialog(
-      //               context: context,
-      //               builder: (ctx) => AlertDialog(
-      //                 title: const Text("Error"),
-      //                 content:  Text(error),
-      //                 actions: <Widget>[
-      //                   TextButton(
-      //                     onPressed: () {
-      //                       Navigator.of(ctx).pop();
-      //                     },
-      //                     child: Container(
-      //                       color: Colors.green,
-      //                       padding: const EdgeInsets.all(14),
-      //                       child: const Text("okay"),
-      //                     ),
-      //                   ),
-      //                 ],
-      //               ),
-      //             );
-      print(err.toString());
-    }
-  }
+  // showDialog(
+  //               context: context,
+  //               builder: (ctx) => AlertDialog(
+  //                 title: const Text("Error"),
+  //                 content:  Text(error),
+  //                 actions: <Widget>[
+  //                   TextButton(
+  //                     onPressed: () {
+  //                       Navigator.of(ctx).pop();
+  //                     },
+  //                     child: Container(
+  //                       color: Colors.green,
+  //                       padding: const EdgeInsets.all(14),
+  //                       child: const Text("okay"),
+  //                     ),
+  //                   ),
+  //                 ],
+  //               ),
+  //             );
+ print(err.toString());
 }
-<<<<<<< HEAD
  }
  
 }
@@ -502,5 +374,3 @@ class Country {
       flagUrl: json['flags']['png'],
     );
   }}
-=======
->>>>>>> 91eb8f265592fa3192ef21397aefe07baaa3d6a3
