9aa80272bd35f31bce1851366f5ea563a2877641
fghonema <jenifer1971@live.com>
2023-01-05 02:00:46 +0200

    error page

--- kargo/lib/components/no_Internet.dart
+++ kargo/lib/components/no_Internet.dart
@@ -1,26 +1,65 @@
 import 'package:flutter/cupertino.dart';
 
 import 'package:flutter/material.dart';
+import 'package:kargo/screens/home_page.dart';
 
 class noInternet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        // backgroundColor: Colors.green[200],
         body: Center(
-            child: Card(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15)),
-                margin: EdgeInsets.all(15),
-                color: Color.fromRGBO(239, 235, 235, 1),
-                borderOnForeground: true,
-                elevation: 4,
-                child: Container(
-                    height: 100,
-                    child: Center(
-                        child: Text("Please Check Your Internet Connection!",
+            child: Container(
+                height: 500,
+                child: Column(children: [
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                    Image(
+                      image: AssetImage('assets/images/pngWifi.png'),
+                      width: 200,
+                      height: 200,
+                    )
+                  ]),
+                  Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 30),
+                        Text("NO INTERNET",
                             style: TextStyle(
                               fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            )))))));
+                              fontWeight: FontWeight.w300,
+                            )),
+                        Text("CONNECTION",
+                            style: TextStyle(
+                              fontSize: 40,
+                              fontWeight: FontWeight.w900,
+                            )),
+                        Text("Please Check Your Connection and Try Again!",
+                            style: TextStyle(
+                              fontSize: 14,
+                              //     fontWeight: FontWeight,
+                            )),
+                        SizedBox(height: 45),
+                      ]),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextButton(
+                          onPressed: () => {Navigator.pushNamed(context, '/')},
+                          child: Text('   Try Again   ',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  //fontWeight: FontWeight.bold,
+                                  fontSize: 15)),
+                          style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.all(Colors.black),
+                              shape: MaterialStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(15.0),
+                                      side: BorderSide(color: Colors.black)))))
+                    ],
+                  )
+                ]))));
   }
 }
